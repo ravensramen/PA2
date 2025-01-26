d4ef5a360274ce84a54d22addfb6deeddd01ec4b
ravensramen <153202736+ravensramen@users.noreply.github.com>
2025-01-25 19:28:18 -0800

    Update main.c

--- main.c
+++ main.c
@@ -4,49 +4,43 @@
 
 int main() {
 	printf("Welcome to bootleg spotify - spootify!\n");
+
+	printf("Here are your options, enter the coresponding number to make a choice: \n\n");
 	
-	printf("Here are your options, enter the coresponding number to make a choice: \n");
-	int choice = displayMenu();
 	FILE* input = fopen("musicPlayList.csv", "r");
+	Node* pHead = NULL;
+	int choice = 0;
 
+	do {
+		choice = displayMenu();
+		switch (choice) {
+		case 1:
+			//function 1
+			//Load function
+			loadMusic(input,&pHead); //send library to be read into structs
+			system("pause");
+			system("cls"); //clear console after each run 
+			continue;
+		case 2:
+			printList(pHead);
+			//function 2 DISPLAY
+			continue;
+		case 3:
+			//function 3 EDIT
+			continue;
+		case 4:
+			//function 4 RATE
+			continue;
+		case 5:
+			//function 5 PLAY
+			continue;
+		case 6:
+			//function 6 EXIT
+			break;
 
-	switch (choice) {
-	case 1:
-		//function 1
-		//Load function
-		loadMusic(); //send library to be read into structs
-		break;
-	case 2: 
-		//function 2
-		break;
-	case 3: 
-		//function 3
-		break;
-	case 4:
-		//function 4
-		break;
-	case 5:
-		break;
-	case 6:
-		//function 1
-		break;
-	case 7:
-		//function 2
-		break;
-	case 8:
-		//function 3
-		break;
-	case 9:
-		//function 4
-		break;
-	case 10:
-		break;
-	case 11:
-
-		//exit funct
-		break;
-
-	}
+		}
+		
+	} while (choice != 6);
 
 	//CLOSE FILE
 
